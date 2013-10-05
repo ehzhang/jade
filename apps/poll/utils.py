4d3d0d83360490e37e40abd030cf197200e3594a
Jin Pan <jinpan@mit.edu>
2013-10-05 17:34:18 -0400

    created card, display comment classes

---
+++ apps/poll/utils.py
@@ -0,0 +1,36 @@
+from jade.apps.poll.models import Comment
+from jade.apps.poll.models import Option
+
+
+class Card(object):
+    option_id = None
+    text = None
+    submitter = None
+    votes = None
+    comments = None
+
+    def __init__(self, option_id):
+        self.option_id = option_id
+        option = Option.objects.get(id=option_id)
+
+        self.text = option.text
+        self.submitter = option.submitter
+        self.votes = option.get_votes()
+        self.comments = [
+            DisplayComment(comment) for comment in
+            Comments.objects.filter(option=option)
+        ]
+
+
+class DisplayComment(object):
+    comment_id = None
+    text = None
+    submitter = None
+    votes = None
+    
+    def __init__(self, comment):
+        self.comment_id = comment.id
+        self.text = comment.text
+        self.submitter = comment.submitter
+        self.votes = comment.votes
+
