d31e569e4c217b789ce5991fc202d5a32a7efe0b
Jin Pan <jinpan@mit.edu>
2013-10-05 16:24:39 -0400

    + create poll

--- apps/poll/urls.py
+++ apps/poll/urls.py
@@ -5,4 +5,9 @@ from django.conf.urls import url
 
 urlpatterns = patterns('jade.apps.poll.views',
     url(r'^(?P<poll_id>\d+)/$', 'poll_atomic', name='poll_atomic'),
+    url(r'^create/$', 'create_poll'),
+    url(r'^option_upvote/$', 'option_upvote'),
+    url(r'^option_downvote/$', 'option_downvote'),
+    url(r'^comment_upvote/$', 'comment_upvote'),
+    url(r'^comment_downvote/$', 'comment_downvote'),
 )
--- apps/poll/views.py
+++ apps/poll/views.py
@@ -1,11 +1,20 @@
 from json import dumps
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from jade.apps.poll.models import Comment
 from jade.apps.poll.models import Option
 from jade.apps.poll.models import Poll
+
+
+def JSONResponse(data):
+    return HttpResponse(
+        dumps(data),
+        content_type='application/json'
+    )
+
 
 def poll_atomic(request, poll_id):
     
@@ -16,17 +25,28 @@ def poll_atomic(request, poll_id):
 
     return render(request, 'poll.html', context)
 
+
+def create_poll(request):
+    name = request.POST.get('name') or request.GET.get('name')
+    poll = Poll.objects.create(name=name)
+    poll.save()
+
+    data = {
+        'result': 'success',
+        'url': reverse('poll_atomic', args=(poll.id, ))
+    }
+    return JSONResponse(data)
+
+
 def option_upvote(request, option_id):
+    import ipdb; ipdb.set_trace()
     option = Option.objects.get(id=option_id)
     option.upvote()
 
-    response = {
+    data = {
         'result': 'success',
     }
-    return HttpResponse(
-        dumps(response),
-        content_type='application/json',
-    )
+    return JSONResponse(data)
 
 def option_downvote(request, option_id):
     pass
